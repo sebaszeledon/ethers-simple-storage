80c196c55dcf0ba474eedd87ac1f7111deea7e30
sebaszeledon <sebas.zeledon@gmail.com>
2024-03-28 15:37:14 -0600

    update

--- deploy.js
+++ deploy.js
@@ -1,3 +1,15 @@
-function hi() {
+// synchronous [solidity]
+// asynchronous [javascript] 6:46
+
+async function main() {
   console.log("hi");
+  let variable = 5;
+  console.log(variable);
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
