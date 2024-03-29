2ef1eaeb8a42b3a5843cb91a71b33c9c52a887d9
sebaszeledon <sebas.zeledon@gmail.com>
2024-03-29 13:05:18 -0600

    update

--- deploy.js
+++ deploy.js
@@ -1,16 +1,26 @@
 // synchronous [solidity]
 // asynchronous [javascript] 6:46
 const ethers = require("ethers");
+const fs = require("fs-extra");
 
 async function main() {
   // http://127.0.0.1:7545
   const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
+    "HTTP://127.0.0.1:7545"
   );
   const wallet = new ethers.Wallet(
     "0x2672242fbef4d055cd3c48a5a54338acd6eb87b78bd7ed4706ce4a2024a61c5f",
     provider
   );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy(); //Stop here until the contract is deployed.
+  console.log(contract);
 }
 
 main()
