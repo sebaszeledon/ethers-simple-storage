766d535a4a28d8b07e93b6311efefe1c0259387a
sebaszeledon <sebas.zeledon@gmail.com>
2024-03-29 11:56:47 -0600

    update with gitignore

--- deploy.js
+++ deploy.js
@@ -1,7 +1,17 @@
 // synchronous [solidity]
 // asynchronous [javascript] 6:46
+const ethers = require("ethers");
 
-async function main() {}
+async function main() {
+  // http://127.0.0.1:7545
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "0x2672242fbef4d055cd3c48a5a54338acd6eb87b78bd7ed4706ce4a2024a61c5f",
+    provider
+  );
+}
 
 main()
   .then(() => process.exit(0))
