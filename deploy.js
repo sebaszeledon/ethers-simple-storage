08c871959913e2f01e1e4b7b180d413934915b8a
sebaszeledon <sebas.zeledon@gmail.com>
2024-04-01 21:45:36 -0600

    update

--- deploy.js
+++ deploy.js
@@ -1,35 +1,36 @@
 // synchronous [solidity]
 // asynchronous [javascript] 7:34
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
 
 async function main() {
-  // http://127.0.0.1:7545
-  console.log(process.env.RPC_URL);
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-  const binary = fs.readFileSync(
-    "./SimpleStorage_sol_SimpleStorage.bin",
-    "utf8"
-  );
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
-  const contract = await contractFactory.deploy({ gasLimit: 6721975 });
-  await contract.deployTransaction.wait(1);
-  // Get Number
-  const currentFavoriteNumber = await contract.retrieve();
-  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
-  const transactionResponse = await contract.store("8");
-  const transactionReceipt = await transactionResponse.wait(1);
-  const updatedFavoriteNumber = await contract.retrieve();
-  console.log(`Updated favorite number is: ${updatedFavoriteNumber}`);
+    // http://127.0.0.1:7545
+    console.log(process.env.RPC_URL)
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8",
+    )
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    console.log("Deploying, please wait...")
+    const contract = await contractFactory.deploy({ gasLimit: 6721975 })
+    await contract.deployTransaction.wait(1)
+    console.log(`Contract Address : ${contract.address}`)
+    // Get Number
+    const currentFavoriteNumber = await contract.retrieve()
+    console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`)
+    const transactionResponse = await contract.store("8")
+    const transactionReceipt = await transactionResponse.wait(1)
+    const updatedFavoriteNumber = await contract.retrieve()
+    console.log(`Updated favorite number is: ${updatedFavoriteNumber}`)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
